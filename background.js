0d2f7033c41173be91f9c9344fa8800e8f7b637e
Adam Loving <adamloving@gmail.com>
2012-12-02 22:53:52 -0800

    on page load

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 var data = {};
 
+// http://developer.chrome.com/extensions/history.html#event-onVisited
+function onVisited(hi) {
+  console.log('onVisited', hi.url);
+  // tell content script to re-scan the page
+
+
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.sendMessage(tab.id, {},  function(response) { console.log('sent!'); });
+  });
+}
+
 function onMessage(request, sender, sendResponse) {
-  console.log(sender.tab ?
-              "from a content script:" + sender.tab.url :
-              "from the extension");
-  
+  console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
   data[request.data.key] = request.data.value;
   console.log("got", request);
 }
@@ -13,13 +21,25 @@ function onMessage(request, sender, sendResponse) {
 
 // Listen for the content script to send a message to the background page.
 console.log('loaded background');
-chrome.extension.onMessage.addListener(onMessage);
 
-// var views = chrome.extension.getViews();
-// for (var i = 0; i < views.length; i++) {
-//   var view = views[i];
-//   view.console.log('duh', view);
-// }
+chrome.extension.onMessage.addListener(onMessage);
+chrome.history.onVisited.addListener(onVisited);
+
 
 
 // chrome.browserAction.onClicked.addListener(function(tabs.Tab tab) {...});
+
+  // var views = chrome.extension.getViews();
+  // for (var i = 0; i < views.length; i++) {
+  //   var view = views[i];
+  //   view.console.log('duh', view);
+  // }
+
+
+//   var canvas = document.getElementById('canvas');
+// var context = canvas.getContext('2d');
+// // ...draw to the canvas...
+// var imageData = context.getImageData(0, 0, 19, 19);
+// chrome.browserAction.setIcon({
+//   imageData: imageData
+// });
--- content_script.js
+++ content_script.js
@@ -1,4 +1,12 @@
-console.log('running');
+/* seems to only run once */
+
+console.log('content script running');
+
+function onMessage(request, sender, sendResponse) {
+  // console.log(sender.tab); 
+  console.log("content script got", request);
+  scanPage();
+}
 
 // regex = /page_id=([0-9]*)/
 // m = regex.exec(document.body.innerHTML);
@@ -16,23 +24,32 @@ console.log('running');
 //   console.log('no match')
 // }
 
-var profilePics = document.getElementsByClassName('profilePic');
-var urls = [];
+function scanPage() {
+  
+  var els = [];
+  var profilePics = document.getElementsByClassName('profilePic');
+  for (i in profilePics) { els.push(profilePics[i]); }
+  profilePics = document.getElementsByClassName('UFIActorImage');
+  for (i in profilePics) { els.push(profilePics[i]); }
 
+  var urls = [];
 
-for (i in profilePics) {
-  var url = profilePics[i].src;
-  console.log(url);
-  urls.push(url);
+  for (i in els) {
+    var url = els[i].src;
+    console.log(url);
+    urls.push(url);
+  }
+
+  chrome.extension.sendMessage({
+      data: {
+        key: 'urls',
+        value: urls      
+      }
+    }, 
+    function(response) {
+      console.log('sent!');
+    }
+  );
 }
 
-chrome.extension.sendMessage({
-    data: {
-      key: 'urls',
-      value: urls      
-    }
-  }, 
-  function(response) {
-    console.log('sent!');
-  }
-);
+chrome.extension.onMessage.addListener(onMessage);
